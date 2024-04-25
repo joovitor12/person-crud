0f17a1fe946d25ec1017bb972e0281772f819574
joao.machado <joao.machado@stilingue.com.br>
2024-04-24 22:08:04 -0300

    chore: add custom json serialization

--- src/main/java/com/person_crud/vo/v1/PersonVO.java
+++ src/main/java/com/person_crud/vo/v1/PersonVO.java
@@ -1,5 +1,8 @@
 package com.person_crud.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,18 +10,22 @@ import java.io.Serializable;
 
 @Getter
 @Setter
+@JsonPropertyOrder({"id", "address", "firstName", "lastName", "gender"})
 public class PersonVO implements Serializable {
 
         private static final long serialVersionUID = 1L;
 
         private Long id;
 
+        @JsonProperty("first_name")
         private String firstName;
 
+        @JsonProperty("last_name")
         private String lastName;
 
         private String address;
 
+        @JsonIgnore
         private String gender;
 
         public PersonVO() {
